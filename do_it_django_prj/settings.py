142ec111ca906f67b2cfcbd31ea94f6c3da10480
JunBaek Kim <kjb112316@gmail.com>
2025-07-02 18:45:53 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0e5be*=4(kw=(*(_jkw0_z#lhibaeyr+4=z&r240^%^g&lh51q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
